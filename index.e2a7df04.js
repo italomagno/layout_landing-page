01b10b71ff9596f769eb092f3dc4c4f6ed12f65c
Italo Magno <italomagno10@gmail.com>
2024-11-26 13:31:25 -0300

    make build

---
+++ index.e2a7df04.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#menu"),t=document.querySelector("html"),c=document.querySelector(".header__nav_menu");document.querySelector(".menu__header_close"),c.addEventListener("click",function(){e.classList.toggle("menu--open"),e.classList.toggle("menu"),t.classList.toggle("no-scroll")});
+//# sourceMappingURL=index.e2a7df04.js.map
